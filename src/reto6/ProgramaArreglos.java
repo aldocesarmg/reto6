2bde4791e6e7000f8637b88bb4f07aae0d739d64
aldocesarmg <aldocesarmg@gmail.com>
2019-03-03 20:59:53 -0600

    Creacion de clase ProgramaArreglos junto con sus metodos

---
+++ src/reto6/ProgramaArreglos.java
@@ -0,0 +1,16 @@
+
+package reto6;
+
+public class ProgramaArreglos {
+    public int cantidad;
+    public String nombres;
+    private boolean esValido;
+    
+    public static void PrimerMetodoArregloU(int[] a){
+        
+    }
+    
+    public static void SegundoMetodoArregloM(int[] b){
+        
+    }
+}
